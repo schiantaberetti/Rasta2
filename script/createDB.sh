4f6721f94acb8f489053f38b378bc4e2a18a86b4
Baldo <baldo@lilik.it>
2012-06-02 17:42:57 +0200

    Improved the createDB script. (now with mutex!)

--- script/createDB.sh
+++ script/createDB.sh
@@ -4,7 +4,8 @@ EXT="jpg"
 SQLITE_DB="database.db"
 SIFT_CMD="../tools/img2sifts"
 JOBS=0
-CONCURRENCY=1
+CONCURRENCY=8
+LOCK_DIR="/tmp/Rasta2.createDB.lock" # mutex
 
 
 function fatal_error() {
@@ -32,18 +33,25 @@ function sqlite3_query(){
 	done
 }
 function extract_sift_job() {
-	while [ $JOBS -ge $CONCURRENCY ]; do sleep 1; done;
-	let JOBS=JOBS+1
-	
 	image_l=$1 # INPUT
 	id_pages_l=$2 # INPUT
 	
+	while [ $JOBS -ge $CONCURRENCY ]; do sleep 1; done;
+
+	while ! mkdir $LOCK_DIR >/dev/null 2>/dev/null ; do sleep 1;done; # Aqcuiring lock
+	let JOBS=JOBS+1
+	rm -R $LOCK_DIR # Releasing lock
+	
+	print_status "Processing SIFT: $image_l" DEBUG;
 	sift_file_name="`echo $image | cut -d '.' -f 1`.sift"
 	sift_file_name="`readlink -f $DB_DIR/sift/`/$sift_file_name"
 
 	$SIFT_CMD `readlink -f "$DB_DIR/pdf_img/$image_l"` $sift_file_name
 	sqlite3_query "$DB_DIR/$SQLITE_DB" "INSERT INTO sifts (name,path,id_pages) VALUES ('`echo $image_l | cut -d '.' -f 1`.sift','$DB_DIR/sift/','$id_pages_l')";
+	
+	while ! mkdir $LOCK_DIR >/dev/null 2>/dev/null ; do sleep 1;done; # Aqcuiring lock
 	let JOBS=JOBS-1
+	rm -R $LOCK_DIR # Releasing lock
 }
 
 if [ $# -gt 0 ]; then
@@ -55,7 +63,7 @@ print_status "Analyzing database in directory: $DB_DIR" DEBUG
 if [ ! -d "$DB_DIR/pdf" ];then
 	fatal_error "Folder <<$DB_DIR/pdf>> doesn't exist."
 fi
-if [ ! `ls -A $DB_DIR/pdf/*.pdf` ];then
+if [ `ls -A $DB_DIR/pdf/*.pdf | wc -l` -le 0 ];then
 	fatal_error "The directory $DB_DIR/pdf is empty, there is no documents to process."
 fi
 if [ ! -x $SIFT_CMD ];then
@@ -72,7 +80,9 @@ fi
 if [ -d $DB_DIR/sift ];then
 	rm -R $DB_DIR/sift
 fi
-
+if [ -d $LOCK_DIR	 ];then
+	rm -R $LOCK_DIR 
+fi
 # INITIALIZE SQULITE3 DB
 print_status "Reinitializing SQLITE DB." DEBUG
 if [ -f "$DB_DIR/$SQLITE_DB" ];then
@@ -105,7 +115,7 @@ for pdf_file in `ls -1 $DB_DIR/pdf/*.pdf | sed 's#.*/##' `; do
 	pdf_basename=`echo $pdf_file| cut -d '.' -f 1`
 	# Extracting SIFT
 	for image in `ls -1  $DB_DIR/pdf_img/$pdf_basename*.$EXT | sed 's#.*/##' `; do
-		print_status "Extracting SIFT from: $image" DEBUG;
+		print_status "Processing image: $image" DEBUG;
 		num_page=`echo $image | sed 's/[^0-9]*//' | cut -d '.' -f 1`
 		
 		sqlite3_query "$DB_DIR/$SQLITE_DB" "INSERT INTO pages (number_of_page,id_paper,path, name, id_pages) VALUES ('$num_page','$id_paper','$DB_DIR/pdf_img/','$image','$id_pages')";
@@ -117,6 +127,13 @@ for pdf_file in `ls -1 $DB_DIR/pdf/*.pdf | sed 's#.*/##' `; do
 	let id_paper=id_paper+1
 done
 
+print_status "Waiting for pending jobs..." DEBUG;
+for job in `jobs -p`
+do
+    wait $job || print_status "Failure in waiting for job with pid: $job"
+done
+
+print_status "DB creation completed. :-)" DEBUG;
 
 #print_status "DB creation complete. :-)" VISUAL
 
